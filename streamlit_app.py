fca033432e35d8ec246d7659d154f0452ead7a20
Danish Farooq <44938983+mdfarooq0815@users.noreply.github.com>
2024-09-21 00:12:33 +0500

    ruityvice nutrition information
    New section to display fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,11 @@ if ingredients_list:
         
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 
 
 
